98850d779e42b82230046386233bf03a44fcf79d
tiffcklai <76405486+tiffcklai@users.noreply.github.com>
2021-03-07 15:43:26 -0500

    Webscraping Practice

---
+++ Mission_to_Mars.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[13]:
+
+
+# Import Splinter and BeautifulSoup
+from splinter import Browser
+from bs4 import BeautifulSoup as soup
+
+import pandas as pd
+
+
+# In[2]:
+
+
+# Set the executable path and initialize the chrome browser in splinter
+executable_path = {'executable_path': 'C:/Users/tiffc/chromedriver/chromedriver'}
+browser = Browser('chrome', **executable_path)
+
+
+# In[3]:
+
+
+# Visit the mars nasa news site
+url = 'https://mars.nasa.gov/news/'
+browser.visit(url)
+# Optional delay for loading the page
+browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
+
+
+# In[4]:
+
+
+# Set up html parser
+html = browser.html
+news_soup = soup(html, 'html.parser')
+slide_elem = news_soup.select_one('ul.item_list li.slide')
+
+
+# In[5]:
+
+
+# Begin scraping 
+slide_elem.find("div", class_='content_title')
+
+
+# In[6]:
+
+
+# Use the parent element to find the first `a` tag and save it as `news_title`
+news_title = slide_elem.find("div", class_='content_title').get_text()
+news_title
+
+
+# In[7]:
+
+
+# Use the parent element to find the paragraph text
+news_p = slide_elem.find('div', class_="article_teaser_body").get_text()
+news_p
+
+
+# ### Featured Images
+
+# In[8]:
+
+
+# Visit URL
+url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
+browser.visit(url)
+
+
+# In[9]:
+
+
+# Find and click the full image button
+full_image_elem = browser.find_by_tag('button')[1]
+full_image_elem.click()
+
+
+# In[10]:
+
+
+# Parse the resulting html with soup
+html = browser.html
+img_soup = soup(html, 'html.parser')
+
+
+# In[11]:
+
+
+# Find the relative image url
+img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+img_url_rel
+
+
+# In[12]:
+
+
+# Use the base URL to create an absolute URL
+img_url = f'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/{img_url_rel}'
+img_url
+
+
+# In[14]:
+
+
+df = pd.read_html('http://space-facts.com/mars/')[0]
+df.columns=['description', 'value']
+df.set_index('description', inplace=True)
+df
+
+
+# In[15]:
+
+
+# Convert DataFrame back to HTML-ready code 
+df.to_html()
+
+
+# In[16]:
+
+
+browser.quit()
+
